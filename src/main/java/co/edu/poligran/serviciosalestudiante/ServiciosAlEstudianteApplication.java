d405155a1464b2d51513b7297d4a3f82b9cad85e
Antonio Paternina <acpaternina@poli.edu.co>
2017-02-11 18:56:46 -0500

    corrección de errores
    + se añade dependencia de H2 para uso en ambiente local y corregir
    problema al ejecutar tests de JUnit.
    + se renombra paquete base de la aplicación para hacerlo más específico.

--- src/main/java/co/edu/poligran/serviciosalestudiante/ServiciosAlEstudianteApplication.java
+++ src/main/java/co/edu/poligran/serviciosalestudiante/ServiciosAlEstudianteApplication.java
@@ -1,4 +1,4 @@
-package co.edu.poligran;
+package co.edu.poligran.serviciosalestudiante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/co/edu/poligran/serviciosalestudianteco/edu/poligran/ServiciosAlEstudianteApplicationTests.java
+++ src/test/java/co/edu/poligran/serviciosalestudianteco/edu/poligran/ServiciosAlEstudianteApplicationTests.java
@@ -1,4 +1,4 @@
-package co.edu.poligran;
+package co.edu.poligran.serviciosalestudianteco.edu.poligran;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
