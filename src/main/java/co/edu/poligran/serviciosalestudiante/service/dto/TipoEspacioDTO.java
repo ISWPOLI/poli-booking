f593925e052f4880e2363a5584c3fcc60a7e628a
Antonio Paternina <acpaternina@poli.edu.co>
2017-05-06 13:35:55 -0500

    usar herramienta de análisis estático de código #132
    + se corrigen los 6 errores de más alta prioridad (P1) en la siguiente categoría:
    
    Malicious code vulnerability
    Mutable static field
    Field isn't final but should be

--- src/main/java/co/edu/poligran/serviciosalestudiante/service/dto/TipoEspacioDTO.java
+++ src/main/java/co/edu/poligran/serviciosalestudiante/service/dto/TipoEspacioDTO.java
@@ -1,12 +1,12 @@
 package co.edu.poligran.serviciosalestudiante.service.dto;
 
 public class TipoEspacioDTO extends BaseDTO {
-    public static String CUBICULO_ESTUDIO = "CUBICULO_ESTUDIO";
-    public static String CUBICULO_VIDEO = "CUBICULO_VIDEO";
-    public static String GIMNASIO = "GIMNASIO";
-    public static String CANCHA_TENIS = "CANCHA_TENIS";
-    public static String CANCHA_MULTIPLE = "CANCHA_MULTIPLE";
-    public static String CANCHA_FUTBOL = "CANCHA_FUTBOL";
+    public static final String CUBICULO_ESTUDIO = "CUBICULO_ESTUDIO";
+    public static final String CUBICULO_VIDEO = "CUBICULO_VIDEO";
+    public static final String GIMNASIO = "GIMNASIO";
+    public static final String CANCHA_TENIS = "CANCHA_TENIS";
+    public static final String CANCHA_MULTIPLE = "CANCHA_MULTIPLE";
+    public static final String CANCHA_FUTBOL = "CANCHA_FUTBOL";
 
     private String nombre;
 
