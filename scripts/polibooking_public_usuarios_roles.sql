6bc7a9c0fe54a57c3b6829ee4a91ef764a7a3840
Antonio Paternina <acpaternina@poli.edu.co>
2017-05-16 22:12:42 -0500

    backup de base de datos QA

--- scripts/polibooking_public_usuarios_roles.sql
+++ scripts/polibooking_public_usuarios_roles.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.usuarios_roles (user_id, user_role_id) VALUES (316, 1);
-INSERT INTO public.usuarios_roles (user_id, user_role_id) VALUES (317, 3);
 INSERT INTO public.usuarios_roles (user_id, user_role_id) VALUES (4704, 3);
+INSERT INTO public.usuarios_roles (user_id, user_role_id) VALUES (317, 3);
