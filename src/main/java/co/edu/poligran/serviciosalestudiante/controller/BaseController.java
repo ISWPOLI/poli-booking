1acc2b20b966e15b7a9a56d188af53c6e7dce2cc
Antonio Paternina <acpaternina@poli.edu.co>
2017-05-16 21:36:53 -0500

    usar herramienta de análisis estático de código #132
    + se corrige 1 bug de la siguiente categoría:
    Unread field
    Unread public/protected field

--- src/main/java/co/edu/poligran/serviciosalestudiante/controller/BaseController.java
+++ src/main/java/co/edu/poligran/serviciosalestudiante/controller/BaseController.java
@@ -1,7 +1,5 @@
 package co.edu.poligran.serviciosalestudiante.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -9,8 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 
 public abstract class BaseController {
 	public static final String FORMATO_FECHA_POR_DEFECTO = "yyyy-MM-dd";
-
-	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	protected String getUsuarioEnSesion() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
